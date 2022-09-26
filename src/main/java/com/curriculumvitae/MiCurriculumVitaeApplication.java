10a604a38995fa6828c045e14c08228009ab8179
Noevd <noeliav.96@gmail.com>
2022-09-26 00:13:42 -0300

    Initial commit

---
+++ src/main/java/com/curriculumvitae/MiCurriculumVitaeApplication.java
@@ -0,0 +1,13 @@
+package com.curriculumvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCurriculumVitaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCurriculumVitaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curriculumvitae/MiCurriculumVitaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculumvitae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCurriculumVitaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
